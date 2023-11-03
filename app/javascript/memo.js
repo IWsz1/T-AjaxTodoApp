6571271af7cff1883367f40499d7ce653633464d
IWsz1 <soraa24926@gmail.com>
2023-11-03 15:18:02 +0900

    jsのメモ追加投稿機能

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,3 +1,19 @@
+const buildHTML = (XHR) => {
+      // レスポンスプロパティでレスポンスで届いたデータが確認できる  XMLHttpRequestオブジェクトのプロパティの一つ
+      const item = XHR.response.post;
+      // HTMLに入れ込む要素を作成
+      const html = `
+        <div class="post">
+          <div class="post-date">
+            投稿日時：${item.created_at}
+          </div>
+          <div class="post-content">
+            ${item.content}
+          </div>
+        </div>`;
+  return html;
+};
+
 function post (){
   const form = document.getElementById("form");
   // 第二引数のeでイベント発生時の情報を持った物（これもデフォルトのオブジェクト）
@@ -15,10 +31,30 @@ function post (){
     XHR.responseType = "json";
     // XHRを用いてFormDataをリクエストとして送信
     XHR.send(formData);
+    //onloadはリクエストが成功した時に呼び出される XMLHttpRequestオブジェクトのプロパティの一つ
+    XHR.onload = () => {
+      // レスポンスに問題があったときの処理
+      // ステータスが200以外だったら何らかの通信エラー
+      if (XHR.status != 200) {
+        // アラートでステータスコードと通信エラーに関するテキストを表示
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        // jsの処理から強制的に抜ける
+        return null;
+      };
+
+      // jsで作ったhtmlを入れ込む元要素
+      const list = document.getElementById("list");
+      // list要素の後ろにbuildHTMLで作成したhtmlを配置 (引数はbuildHTMLで使用する非同期通信オブジェクト)
+      list.insertAdjacentHTML("afterend", buildHTML(XHR));
+      // メモを入力するフォーム要素を取得
+      const formText = document.getElementById("content");
+      // 入力スペースの値を空に変更
+      formText.value = "";
+    };
   });
 
 
-  console.log("イベント発火");
 };
-
+// turboは非同期のいろんなことをしてるもの
 window.addEventListener('turbo:load', post);
+
