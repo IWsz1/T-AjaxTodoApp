e2fb8f77d1e321eb9f3d035ab163fa1e63c02377
IWsz1 <soraa24926@gmail.com>
2023-11-03 14:12:40 +0900

    controllerでの処理作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,13 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index
+    # Post.create(content: params[:content])
+    # redirect_to action: :index
+
+    # contentにparamsのcontentキーの中に入った情報を格納してレコードを作成
+    # 作成したレコードをpostに代入
+    post = Post.create(content: params[:content])
+    # postというキーにレコードのデータを入れてjsonで返す
+    render json:{ post: post }
   end
 end
